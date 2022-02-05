a8db5ec7ef3ba8ee401594f946588207d1a5b303
Bradley Marques <bradleyrcmarques@gmail.com>
2022-02-05 09:47:33 +0200

    User has to have unique username

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,23 @@ class UserTest < ActiveSupport::TestCase
     user = FactoryBot.build(:user, username: nil)
     refute(user.valid?)
   end
+
+  test "a user is valid and can be saved with a username" do
+    username = "billybob@example.com"
+    user = FactoryBot.build(:user, username: username)
+    assert(user.valid?)
+    assert(user.save)
+  end
+
+  test "a user has to have a unique username" do
+    username = "billybob@example.com"
+
+    user_1 = FactoryBot.build(:user, username: username)
+    assert(user_1.valid?)
+    assert(user_1.save)
+
+    user_2 = FactoryBot.build(:user, username: username)
+    refute(user_2.valid?)
+    refute(user_2.save)
+  end
 end
