0216c26ed4d5fb6cec5c8bf1208816a893a2c898
Bradley Marques <bradleyrcmarques@gmail.com>
2022-02-06 10:48:21 +0200

    More controller tests

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,13 +2,20 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
+        respond_to do |format|
+          format.json do
+            books = Book.all.order(:title)
+            serializer = BookSerializer.new(books)
+
+            render(status: :ok, json: serializer.serializable_hash)
+          end
+        end
       end
 
       def show
         respond_to do |format|
           format.json do
             book = Book.find(params.require(:id))
-
             serializer = BookSerializer.new(book)
 
             render(status: :ok, json: serializer.serializable_hash)
--- test/controllers/api/v1/books_controller_test.rb
+++ test/controllers/api/v1/books_controller_test.rb
@@ -53,6 +53,64 @@ module Api
 
         assert_equal(expected_payload, actual_payload)
       end
+
+      test "an unauthenticated user obtains the correct JSON API formatted payload
+          when indexing multiple books" do
+        title_1 = "Book A"
+        title_2 = "Book B"
+
+        book_1 = FactoryBot.create(:book, title: title_1)
+        user_1 = book_1.user
+        book_2 = FactoryBot.create(:book, title: title_2)
+        user_2 = book_2.user
+
+        get(api_v1_books_url, as: :json)
+
+        assert(:ok)
+
+        expected_payload = {
+          data: [
+            {
+              id: book_1.id,
+              type: "book",
+              attributes: {
+                title: book_1.title,
+                description: book_1.description,
+                price_usd: book_1.price_usd
+              },
+              relationships: {
+                user: {
+                  data: {
+                    id: user_1.id,
+                    type: "user"
+                  }
+                }
+              }
+            },
+            {
+              id: book_2.id,
+              type: "book",
+              attributes: {
+                title: book_2.title,
+                description: book_2.description,
+                price_usd: book_2.price_usd
+              },
+              relationships: {
+                user: {
+                  data: {
+                    id: user_2.id,
+                    type: "user"
+                  }
+                }
+              }
+            }
+          ]
+        }.with_indifferent_access
+
+        actual_payload = JSON.parse(response.body).with_indifferent_access
+
+        assert_equal(expected_payload, actual_payload)
+      end
     end
   end
 end
